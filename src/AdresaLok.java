81fb41a76dbce1e902098dab49e268763ac1f045
Zmijaa <mircevfilip98@gmail.com>
2024-01-11 10:57:25 +0100

    first commit

---
+++ src/AdresaLok.java
@@ -0,0 +1,15 @@
+class AdresaLok {
+private int stanBr;
+private String lokacija;
+public AdresaLok (int stanBr, String lokacija) {
+	this.stanBr = stanBr;
+	this.lokacija = lokacija;
+	}
+	public int getStanBr(){
+	return stanBr;
+	}
+	public String getLokacija(){
+	return lokacija;
+	}
+	
+}
